786e7236d7bb1cac214ecfd0ff38be16bd46d3ba
MATHEUS SARAGOCA DE CARVALHO <matheus.scarvalho22@senacsp.edu.br>
2024-09-25 15:33:08 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let nota = parseFloat (prompt("insira a sua nota"))
+if (nota >= 7) {
+    alert("você foi aprovado👍😁");
+} else if (nota >=5) {
+    alert("voce esta de recuperação🤏🙁");
+} else {
+    alert("voce foi reprovado👎🥴");
+}
